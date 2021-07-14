052223ad1e467427a1926e63318fc5ab8ebdc509
Damián Rojas <drojas@stilowg.local>
2021-07-14 08:18:04 -0300

    Primer commit de ejercicio Firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+//FIREBASE
+import { db } from './firebase'
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=> {
+    console.log('Getting Data');
+
+  },[])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Firebase App</h1>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBcRCBEiX1c3xqRR53KM2H72zGcbkBOrW8",
+    authDomain: "fir-coderhouse.firebaseapp.com",
+    projectId: "fir-coderhouse",
+    storageBucket: "fir-coderhouse.appspot.com",
+    messagingSenderId: "367348446063",
+    appId: "1:367348446063:web:77b20312a7bc7a1d84ee9a"
+  };
+  // Initialize Firebase
+const fb = firebase.initializeApp(firebaseConfig);
+
+export const db = fb.firestore();
